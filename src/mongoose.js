0c641b2f0ea2455fd3b7a85472b2438afb96099b
unknown <jashwanthbj9@gmail.com>
2022-12-11 18:41:41 +0530

    deploying db prod url

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,12 +1,31 @@
 import mongoose from "mongoose";
 mongoose
-  .connect("mongodb+srv://jashwanth:jashwanthbj9@chess.pww3bsh.mongodb.net/?retryWrites=true", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://jashwanth:jashwanthbj9@chess.pww3bsh.mongodb.net/?retryWrites=true",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then((res) => {
     console.log("DB Connected!");
   })
   .catch((err) => {
     console.log("Error while connecting to DB", err.message);
   });
+
+const connectDatabase = async () => {
+  try {
+    mongoose.set("useNewUrlParser", true);
+    await mongoose.connect(
+      "mongodb+srv://jashwanth:jashwanthbj9@chess.pww3bsh.mongodb.net/?retryWrites=true"
+    );
+
+    console.log("connected to database");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+connectDatabase();
